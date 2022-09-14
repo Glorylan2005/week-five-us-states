f6c3791628087801f7c6f3f9a5b2ab2cd90dd215
Natali Lohr <nlohr@lohrconstructionco.com>
2022-09-13 23:02:24 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import { data } from './data';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [places, setPlaces] = useState(data);
+const [showText, setShowText] = useState (false);
+
+
+const removeItem = (id) => { 
+let newPlaces = places.filter (place => place.id !==id);
+setPlaces(newPlaces);
+console.log(newPlaces);
+}
+
+const showTextClick = (item) => {
+item.showMore = !item.showMore;
+setShowText(!showText)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className='container'>
+        <h1>{places.length} The Best Places to Visit in the United States</h1>
+      </div>
+
+      {places.map ((item => {
+        const {id, placeName, description, image, attractions, showMore} = item;
+        return(<div key={id}>
+
+                  <div className='container city'>
+                    <h2>{id}. {placeName}</h2>
+                  </div>
+
+                  <div className='container'>
+                    <p className='description'>{showMore ? description : description.substring (0, 150) + " ..."}
+                    <button className='show' onClick ={()=> showTextClick(item)}>{showMore ? "Show less" : "show more"}</button>
+                    </p> 
+                  </div> 
+
+                  <div className='container pic'>
+                    <img src={image} alt={placeName} width="400px" />
+                  </div>
+
+                  <div className='container'>
+                    <p className='attraction'><b>Attraction:</b> {attractions}</p>
+                  </div>
+
+                  <div className='container'>
+                    <button className='btn' onClick={()=>removeItem(id)}>Remove</button>
+                  </div>
+        </div>
+        )
+      }))}
+        <div className='container'>
+          <button className='btn' onClick = {()=> setPlaces([])}>Delete All</button>
+        </div>
+
+      </div>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,80 @@
+export const data = [
+    {id: 1,  
+        placeName: "New York",  
+        description: "New York City is like no other city in the world, and one that must be experienced to be fully appreciated. Sightsee by day, take in a Broadway show in the evening, work in time for shopping or a stroll, and relax and reminisce over a fantastic meal. This is New York. With too much to see and do in a day or a weekend, this is a city worth visiting time and time again.",  
+        image: "https://www.planetware.com/photos-large/USNY/usa-best-places-new-york.jpg",  
+        attractions: "The Empire State Building, Rockefeller Plaza, the Chrysler Building, Central Park, The High Line, Times Square, 5th Avenue, Broadway, the Statue of Liberty.", 
+        showMore: false 
+    },
+
+    {id: 2,  
+        placeName: "San Francisco",  
+        description: "This charming and picturesque West Coast city is the perfect getaway destination for couples, singles, or families. Famous sites, charming streets, beautiful views, and outdoor dining are all part of what makes San Francisco such a great place to visit.",  
+        image: "https://www.planetware.com/photos-large/USNY/usa-best-places-san-francisco.jpg",  
+        attractions: "Take a cruise on San Francisco Bay, tour Alcatraz, drive over the Golden Gate Bridge, wander around Fisherman's Wharf, or hop on a street car to explore the city sites.", 
+        showMore: false 
+    },
+
+    {id: 3,  
+        placeName: "Las Vegas",  
+        description: "This glittering city of lights in the desert holds a unique appeal, and one that has been attracting visitors for decades. Huge resort complexes, with all kinds of things to see and do at any time of year, have made this a destination that draws everyone, from want-to-be newlyweds who come here to say their vows, to families or singles who just want to hang out around a pool.",  
+        image: "https://www.planetware.com/photos-large/USNY/usa-best-places-las-vegas.jpg",  
+        attractions: "Explore in the surrounding area, with the Grand Canyon, Hoover Dam, Death Valley National Park, and Valley of Fire State Park all within easy day-tripping distance. Las Vegas is ideal for a long weekend if you live in Los Angeles or Phoenix.", 
+        showMore: false 
+    },
+
+    {id: 4,  
+        placeName: "Waikiki",  
+        description: "Waikiki is one of America's top beaching destinations, with all the comforts of North America on a beautiful tropical island in the Pacific Ocean. Located on the Hawaiian island of Oahu, Waikiki is a suburb of Honolulu known for the beautiful golden sand beach that stretches along the oceanfront, backed by hotels and retail establishments.",  
+        image: "https://www.planetware.com/photos-large/USNY/usa-best-places-waikiki.jpg",  
+        attractions: "Rent a surfboard and test your skills in the waves, hit the shops when you're done with the beach, and enjoy great dining options in the evening.", 
+        showMore: false 
+    },
+
+    {id: 5,  
+        placeName: "Miami",  
+        description: "The hot spot of southern Florida, Miami is more than just a great beaching destination. Wonderful beaches can be found all over Florida, but Miami offers an atmosphere like no other city in the state.",  
+        image: "https://www.planetware.com/photos-large/USNY/usa-best-places-miami.jpg",  
+        attractions: "For a unique experience take a day trip from Miami to Everglades National Park to see alligators, turtles, and an array of birds in this one-of-a-kind wetlands area.", 
+        showMore: false 
+    },
+    {id: 6,  
+        placeName: "Los Angeles",  
+        description: "Southern California has a culture all of its own, and Los Angeles is the epicenter of this unique area.",  
+        image: "https://www.planetware.com/photos-large/USNY/usa-best-places-los-angeles.jpg",  
+        attractions: "The city has always been associated with glamour, with the suburbs of Hollywood, Beverly Hills, and Bel Air dominating pop culture.", 
+        showMore: false 
+    },
+
+    {id: 7,  
+        placeName: "New Orleans",  
+        description: "New Orleans is like no other southern city. The mix of cultures, with strong French and Spanish influences, is more reminiscent of the Caribbean than the United States. Cajun and Creole cuisine, jazz music, and the architecture of the French Quarter set this city apart.",  
+        image: "https://www.planetware.com/photos-large/USNY/usa-best-places-new-orleans.jpg",  
+        attractions: "The highlight of New Orleans' calendar year is Mardi Gras, when elaborate costumes and outrageous floats light up the streets.", 
+        showMore: false 
+    },
+
+    {id: 8,  
+        placeName: "Santa Fe",  
+        description: "A melting pot of Spanish, Native American, Mexican, and Anglo-Americans, Santa Fe has a unique vibe that, once experienced, tends to draw one back time and time again. This historical city's pleasant streets, lined by Spanish colonial churches, adobe homes, and towering trees, just beg to be photographed.",  
+        image: "https://www.planetware.com/wpimages/2022/02/united-states-best-places-to-visit-santa-fe.jpg",  
+        attractions: "Santa Fe is ideally positioned close to the mountains, where you'll find some of the best skiing in New Mexico at resorts including Taos, Pajarito, and Siapu.", 
+        showMore: false 
+    },
+
+    {id: 9,  
+        placeName: "Boston",  
+        description: "Located along the Eastern Seaboard, Boston is a city of history. The famous Freedom Trail leads past some of the city's most famous and historic sites, making sightseeing and navigating through the city extremely easy.",  
+        image: "https://www.planetware.com/photos-large/USNY/usa-best-places-boston.jpg",  
+        attractions: "Modern skyscrapers stand next to centuries-old buildings, and open public spaces, like Boston Common, give the city a quaint, small-town feel. The city also has a strong arts and cultural scene, and is home to the famous Boston Pops Orchestra.", 
+        showMore: false 
+    },
+
+    {id: 10,  
+        placeName: "Chicago",  
+        description: "Set on the shores of Lake Michigan, Chicago is a great summer destination, with parks and a beautiful waterfront. But its arts and cultural attractions, as well as fantastic shopping, make it a popular year-round destination.",  
+        image: "https://www.planetware.com/photos-large/USNY/usa-best-places-chicago.jpg",  
+        attractions: " Millennium Park, the Art Institute of Chicago, Navy Pier, and the views from the Willis Tower SkyDeck.", 
+        showMore: false 
+    }
+]
